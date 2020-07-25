b1d473dac73cd6f9ad3bf166041373aa3fef3ae4
Subhasis Laha <subh.laha@gmail.com>
2020-07-25 14:32:11 +0530

    Updates

--- precache-manifest.4abf382342912559bb55b362dac62c9a.js
+++ precache-manifest.4abf382342912559bb55b362dac62c9a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d6f65c5a597f10ade2424d0d334a865a",
+    "revision": "7e0f555d5cc0a3c989c41e30afd0f465",
     "url": "/test-react-app-1/index.html"
   },
   {
-    "revision": "a08f1d9988b1a1067e76",
+    "revision": "1646c0810dd699c491dd",
     "url": "/test-react-app-1/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/test-react-app-1/static/js/2.9424a200.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a08f1d9988b1a1067e76",
-    "url": "/test-react-app-1/static/js/main.5383367c.chunk.js"
+    "revision": "1646c0810dd699c491dd",
+    "url": "/test-react-app-1/static/js/main.500d1047.chunk.js"
   },
   {
     "revision": "b7294f3b6a1371b44c38",
