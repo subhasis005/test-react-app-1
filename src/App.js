81da3510f2efbff0c9f631b9fcf8d17fff37f730
Subhasis Laha <subh.laha@gmail.com>
2020-07-25 14:39:21 +0530

    Remodified App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter basename="/test-react-app-1">
+    <BrowserRouter>
       <Nav />
       <Myroute />
     </BrowserRouter>
