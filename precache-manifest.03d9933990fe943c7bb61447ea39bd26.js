fa6ef4c511574c2f2ca1ca794b07151a0b591c41
Subhasis Laha <subh.laha@gmail.com>
2020-07-25 14:13:59 +0530

    Updates

---
+++ precache-manifest.03d9933990fe943c7bb61447ea39bd26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f65c5a597f10ade2424d0d334a865a",
+    "url": "/test-react-app-1/index.html"
+  },
+  {
+    "revision": "a08f1d9988b1a1067e76",
+    "url": "/test-react-app-1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "76291b33386b4b227d5d",
+    "url": "/test-react-app-1/static/js/2.9424a200.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-react-app-1/static/js/2.9424a200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a08f1d9988b1a1067e76",
+    "url": "/test-react-app-1/static/js/main.5383367c.chunk.js"
+  },
+  {
+    "revision": "b7294f3b6a1371b44c38",
+    "url": "/test-react-app-1/static/js/runtime-main.f5986298.js"
+  }
+]);
